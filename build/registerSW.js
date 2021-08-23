fffa3085dfa1f7983e7398ad876594852811b17d
vidhatashetty <toriumcar@gmail.com>
2021-08-23 16:59:54 +0530

    fix build

--- build/registerSW.js
+++
@@ -1,5 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./sw.js")
-    .then(res => console.log("registered-player-sw"))
-    .catch(err => console.log("error"));
-}
