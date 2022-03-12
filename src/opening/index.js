e826991ae92ce6d6b623ee3e18cc2333c6b67d19
vidshetty <toriumcar@gmail.com>
2022-03-12 15:16:46 +0530

    response bar changes

--- src/opening/index.js
+++ src/opening/index.js
@@ -17,10 +17,10 @@ import {
 import { ResponseBar } from "../homepage";
 
 
-// Opening
-let requestTimeout = null;
-let responseBarTimeout = null;
-let reloadTimeout = null;
+// // Opening
+// let requestTimeout = null;
+// let responseBarTimeout = null;
+// let reloadTimeout = null;
 
 
 const Opening2 = () => {
@@ -29,35 +29,68 @@ const Opening2 = () => {
     const [redirectValue, setRedirectValue] = useState(false);
     const [, setLoaderClass] = useState("opening-loader hidden");
 
-    const request = async () => {
-        const res = await APIService.activateCheck();
-        if (res.status === "active") {
-            APIService.recordTime();
-            await wait(500);
-            if (requestTimeout === null) return;
-            clearTimeout(requestTimeout);
-            clearTimeout(responseBarTimeout);
-            clearTimeout(reloadTimeout);
-            requestTimeout = null;
-            responseBarTimeout = null;
-            reloadTimeout = null;
-            setRedirectValue(true);
-        }
+    // const request = async () => {
+    //     const res = await APIService.activateCheck();
+    //     if (res.status === "active") {
+    //         APIService.recordTime();
+    //         await wait(500);
+    //         if (requestTimeout === null) return;
+    //         clearTimeout(requestTimeout);
+    //         clearTimeout(responseBarTimeout);
+    //         clearTimeout(reloadTimeout);
+    //         requestTimeout = null;
+    //         responseBarTimeout = null;
+    //         reloadTimeout = null;
+    //         setRedirectValue(true);
+    //     }
+    // };
+
+    // const call = async () => {
+    //     await wait(1000);
+    //     setLoaderClass("opening-loader");
+    //     requestTimeout = setTimeout(request, 0);
+    //     responseBarTimeout = setTimeout(() => {
+    //         requestTimeout = null;
+    //         setResObj(prev => {
+    //             return { ...prev, open: true, msg: "Error connecting, reloading...." }
+    //         });
+    //     }, 4000);
+    //     reloadTimeout = setTimeout(() => {
+    //         window.location.reload();
+    //     }, 6000);
+    // };
+
+    const setErrorOnRepeat = () => {
+        setInterval(() => {
+            setResObj(prev => {
+                return { ...prev, open: true, msg: "Something went wrong, try again later!" };
+            });
+        }, 1000);
     };
 
     const call = async () => {
         await wait(1000);
         setLoaderClass("opening-loader");
-        requestTimeout = setTimeout(request, 0);
-        responseBarTimeout = setTimeout(() => {
-            requestTimeout = null;
-            setResObj(prev => {
-                return { ...prev, open: true, msg: "Error connecting, reloading...." }
-            });
-        }, 4000);
-        reloadTimeout = setTimeout(() => {
-            window.location.reload();
-        }, 6000);
+        let breakOut = false;
+        for (let i=0; i<3 && !breakOut; i++) {
+            (async () => {
+                const res = await APIService.activateCheck();
+                if (res.status === "active") {
+                    APIService.recordTime();
+                    await wait(500);
+                    breakOut = true;
+                    setRedirectValue(true);
+                }
+            })();
+            setTimeout(() => {
+                if (breakOut) return;
+                setResObj(prev => {
+                    return { ...prev, open: true, msg: "Error connecting, trying again...." };
+                });
+            }, 4000);
+            await wait(6000);
+        }
+        if (!breakOut) setErrorOnRepeat();
     };
 
     useEffect(() => {
